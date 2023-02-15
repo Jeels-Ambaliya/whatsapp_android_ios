f6c2c977042dfc474a0269b4505ab97ec747449b
Jeels-Ambaliya <19bmiit062@gmail.com>
2023-02-15 12:55:47 +0530

    Initial commit

---
+++ lib/pages/model/globals.dart
@@ -0,0 +1,83 @@
+class Globals {
+  static bool isAndroid = true;
+
+  static List<Map> myTabs = [
+    {'label': "Chats"},
+    {'label': "Status"},
+    {'label': "Calls"},
+  ];
+
+  static List messageList = [
+    {
+      'pic': "https://cdn.wallpapersafari.com/46/60/gPkfdT.jpg",
+      'name': "Jeels",
+      'message': "Hello Bro 😎",
+      'msgint': "2",
+      'time': "11:45 AM",
+    },
+    {
+      'pic':
+          "https://tse3.explicit.bing.net/th?id=OIP.ojQN2okgPeG7Ixk6nRS_OAHaFM&pid=Api&P=0",
+      'name': "Sahil",
+      'message': "Happy Birthday 🎉",
+      'msgint': "5",
+      'time': "1:25 AM",
+    },
+    {
+      'pic':
+          "https://tse3.mm.bing.net/th?id=OIP.a4vM1vy05RwdJH0qLEJdSQHaE6&pid=Api&P=0",
+      'name': "Isha",
+      'message': "Hello Dude 💖",
+      'msgint': "99",
+      'time': "Sunday",
+    },
+    {
+      'pic':
+          "https://purepng.com/public/uploads/large/purepng.com-man-face-herofaceshumansfrontalhuman-identityman-1421526885661w7dlt.png",
+      'name': "Prince",
+      'message': "Hello Bhai",
+      'msgint': "3",
+      'time': "1:56",
+    },
+    {
+      'pic':
+          "https://tse4.mm.bing.net/th?id=OIP.S0QENpu_sBcWxAj0tCnIpwHaLb&pid=Api&P=0",
+      'name': "Nihal",
+      'message': "Hy Bigbro ",
+      'msgint': "8",
+      'time': "7:15",
+    },
+    {
+      'pic':
+          "https://www.wallpaperflare.com/static/525/915/597/writing-pen-man-boy-wallpaper.jpg",
+      'name': "Boss 👑",
+      'message': "Good Morning ",
+      'msgint': "2",
+      'time': "Sunday",
+    },
+    {
+      'pic':
+          "https://www.wallpaperflare.com/static/522/112/939/friends-cocktails-party-people-wallpaper.jpg",
+      'name': "Mom 😍",
+      'message': "Sent a Photo",
+      'msgint': "1",
+      'time': "12:45",
+    },
+    {
+      'pic':
+          "https://tse3.mm.bing.net/th?id=OIP.HbhhZMu5G7GoI1kF8qv1zQHaEK&pid=Api&P=0",
+      'name': "Tako Shine ⭐",
+      'message': "Niche Av",
+      'msgint': "1",
+      'time': "2:23",
+    },
+    {
+      'pic':
+          "https://tse1.mm.bing.net/th?id=OIP.4Py7MYDlJMhMjiA_gELNdAHaGh&pid=Api&P=0",
+      'name': "Jay",
+      'message': "Hello",
+      'msgint': "5",
+      'time': "5:23",
+    },
+  ];
+}
